0b2f4e2c4ebf67db6c594dbfd6e7b289f4d261ab
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-17 07:50:28 -0700

    Updated function comments for main

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,11 @@
 /* more headers goes there */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
  * int main() - This is the entry point of this program.
  * @void: This argument tells the main function to not return anything.
  *
- * Return (0): The return value for main function
+ * Return: The return value for main function
  */
 int main(void)
 {
