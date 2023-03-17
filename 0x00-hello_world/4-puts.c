eca8f1ac4fbf802e5947c783e43c4015d532d941
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-17 10:57:18 -0700

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of this program.
+ *
+ * Return: The return value for main function which is 0 for success.
+ */
+int main() 
+{
+	char cpgrm[] = "Programming is like building a multilingual puzzle\n";
+	puts(cpgrm);;
+	return 0;
+}
