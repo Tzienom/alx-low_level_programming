7fe22b9ea3f14cda6608add485ab1fdb0d3d8f6f
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-18 15:27:59 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of this program.
+ * void: This argument tells the main function to not return anything.
+ *
+ * Return: The return value of the main function.
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
