f9d6bf19c8d66069d7d62194de10899a47757a52
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-17 19:31:00 -0700

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of this program.
+ * void: This argument tells the main function to not return anything.
+ *
+ * Return: The return value for main function which is 0 if successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
