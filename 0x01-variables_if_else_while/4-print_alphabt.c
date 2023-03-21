3e7fbd6b8f8714b3a5fb5822e8c524a96a299bc0
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-20 17:47:23 -0700

    Updated file to follow Betty Coding Style.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	char alphbt;
 
-	for (alphbt = 'a'; alphbt <= 'z'; alphbt++) {
+	for (alphbt = 'a'; alphbt <= 'z'; alphbt++)
+	{
 		if (alphbt == 'e' || alphbt == 'q')
 			continue;
 		putchar(alphbt);
