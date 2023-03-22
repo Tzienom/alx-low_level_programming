28312354f56e0c649313664adcdae9a132df1021
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-22 07:07:27 -0700

    Added the null character to the output.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,10 +9,11 @@
  */
 int main(void)
 {
-	char c[] = "_putchar\n\0";
+	char c[] = "_putchar";
 	long unsigned int i;
 
 	for (i = 0; i < sizeof(c) / sizeof(c[0]); ++i)
 		_putchar(c[i]);
+	_putchar('\n');
 	return (0);
 }
