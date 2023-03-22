f77585431f4d5a83f4df85ea1c98dd05cb9b3471
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-22 05:53:48 -0700

    Initial commit.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - This is the entry point of this program.
+ * void: This argument tells the main function to not return anything.
+ * _putchar: This custom function prints to the screen.
+ *
+ * Return: The return value for main function which returns 0 if successful.
+ */
+int main(void)
+{
+	char c[15];
+	long unsigned int i;
+
+	strcpy(c, "_putchar");
+	for (i = 0; i < sizeof(c)/sizeof(c[0]); i++)
+		_putchar(c[i]);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project contains files for functions and nested loops and also a custom-made header file.
