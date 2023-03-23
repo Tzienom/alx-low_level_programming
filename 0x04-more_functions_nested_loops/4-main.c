fcdfbdb76676b8a634cbd0a3050ccceeb3768906
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-23 09:13:19 -0700

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * void: argument for main function.
+ *
+ * return: 0 if successful.
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
