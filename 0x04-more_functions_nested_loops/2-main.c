750e40fedffb7eb663427fb8dd3bb6383225cc95
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-23 08:13:47 -0700

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * void: argument for main function.
+ *
+ * return: 0 if successful.
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function for multiplying integers.
+ * a: first argument for mul function.
+ * b: second argument for mul function.
+ *
+ * return: 0 if successful.
+ */
+int mul(int a, int b)
+{
+	return a * b;
+}
