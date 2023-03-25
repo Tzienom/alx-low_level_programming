8e33aacfed875bcffabfc16a3bb40461c7efbcea
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-25 10:10:02 -0700

    Added new file: 10-add.c.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers.
+ * @a: value to be added.
+ * @b: value to be added.
+ *
+ * Return: 0 if successful.
+ */
+int add (int a, int b)
+{
+	return (a + b);
+}
