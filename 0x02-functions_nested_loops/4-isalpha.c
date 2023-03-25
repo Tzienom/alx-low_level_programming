cfb3136f3dc5bdffebb1a6898424d92b08388756
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-25 08:12:57 -0700

    Added new file: 4-isalpha.c.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character.
+ * @c: argument for _isalpha.
+ *
+ * return: 1 if alphabet, and 0 for number.
+ */
+int _isalpha(int c)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 65; i <= 90; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+	for (j = 97; j <= 122; j++)
+	{
+		if (c == j)
+			return (1);
+	}
+	for (k = 48; k <= 57; k++)
+	{
+		if (c == k)
+			return (0);
+	}
+	return (0);
+}
