503852ad7e016b6dcea0700575d9d0d562ec8759
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-29 09:53:07 -0700

    Added new file: 1-strncat.c.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - function that concatenates two strings without the null-terminator.
+ * *dest: pointer to hold concatenated string.
+ * *src: source from which string is concatenated.
+ *
+ * Return: dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return dest;
+}
