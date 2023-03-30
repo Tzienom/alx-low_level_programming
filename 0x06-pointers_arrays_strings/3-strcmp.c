91793d46b55c0c6522a63d812feffda1fd42cc6e
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-30 04:34:23 -0700

    Removed 'int j' from file.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
 
 	i = 0;
-	j = 0;
-	while (s1[i] != s2[j] && (s1[i] && s2[j]) != '\0')
+	while (s1[i] != s2[i] && (s1[i] && s2[i]) != '\0')
 	{
-		return (s1[i] - s2[j]);
+		return (s1[i] - s2[i]);
 		i++;
-		j++;
 	}
 	return (0);
 }
