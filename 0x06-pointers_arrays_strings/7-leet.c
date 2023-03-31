4366a153aed7364ace18049c34a2cdf204f6c26d
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-31 06:02:40 -0700

    Modified file.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,24 +10,20 @@ char *leet(char *str)
 {
 	int i;
 	int j;
-	int length;
+	char repstr[] = "aeotl";
+	char REPSTR[] = "AEOTL";
+	char leet[] = "43071";
 
-	length = 0;
-	while (str[length] != '\0')
-		length++;
-	j = length;
-	for (i = 0; i < j; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = 52;
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = 51;
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = 48;
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = 55;
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = 49;
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == repstr[j] || str[i] == REPSTR[j])
+			{
+			       str[i] = leet[j];
+			       break;
+			}
+		}
 	}
 	return (str);
 }
