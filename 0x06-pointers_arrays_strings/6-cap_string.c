08d29b79c528a6346f05e8ae65fe94e4fec3e4ca
Gabriel Ayuba <ayubagabriel2000@gmail.com>
2023-03-31 19:14:05 +0100

    Modified file to include all possible word separators.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - function to capitalize the first letter of every word.
+ * @str: pointer parameter to be used for string capitalization.
+ *
+ * Return: str.
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int whitesp = '\0';
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((i == 0 || whitesp == ' ' || whitesp == ',' || whitesp == ';'
+|| whitesp == '.' || whitesp == '!' || whitesp == '?' || whitesp == '"' ||
+whitesp == '(' || whitesp == ')' || whitesp == '{' || whitesp == '}'
+|| whitesp == '\t' || whitesp == '\n') && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= ('a' - 'A');
+		}
+		if (str[i] == '\t')
+			str[i] = ' ';
+		whitesp = str[i];
+	}
+	return (str);
+}
+
